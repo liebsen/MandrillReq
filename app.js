84baed714bca08f550b8841b5830de6e3e0f0e64
martin <telemagico@gmail.com>
2017-09-14 20:27:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+var routes = require('./routes/index');
+
+// Set static files
+//app.use(express.static('app'));
+//app.use(bodyParser.json());
+
+// Set routes
+app.use('/', routes);
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening at http://localhost:' + server.address().port);
+});
---
+++ cron.js
@@ -0,0 +1,9 @@
+module.exports = function(){
+    setInterval(function(){
+      console.log('cron');
+      
+
+
+      
+    },60*1000);	
+}
